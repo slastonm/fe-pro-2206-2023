cd0d3fd5d08e6532e169982b2fa9bb15efa38091
Max S <slastonm@gmail.com>
2023-06-22 21:16:06 +0300

    cw01

---
+++ 01/math.js
@@ -0,0 +1,68 @@
+// let firstValue = prompt('Print first value');
+// let firstValue = Number(prompt('Print first value'));
+
+// let seconValue  = Number(prompt('Print second value'));
+// '10' => 10 '10.1' => 10 
+// let convertFirstValue = parseInt(firstValue);
+// let convertFirstValue = parseFloat(firstValue);
+// let convertFirstValue = Number(firstValue);
+
+// console.log(convertFirstValue);
+// console.log(+firstValue + +seconValue);
+// console.log(firstValue + seconValue);
+// console.log(firstValue - seconValue);
+// console.log(firstValue / seconValue);
+// console.log(firstValue * seconValue);
+
+// let summ = (firstValue + seconValue)/2;
+// console.log('Middle value: ', summ);
+
+// === Оператор залишку ======
+ console.log('%');
+ console.log(4 % 2);
+ console.log(4 % 3);
+ console.log(6 % 2);
+ console.log(5 % 3);
+
+
+// ==== short operation =====
+
+let myAge = 30;
+
+// myAge = myAge + 3;
+myAge += 1;
+
+let discunt = 100;
+discunt *= 10;
+console.log(myAge);
+console.log(discunt);
+
+let decrementValue = 10;
+decrementValue++;
+// ++decrementValue;
+console.log(decrementValue);
+
+
+let aa = 1;
+let bb = 1;
+console.log('Постфіксний інкремент');
+console.log(aa); //1
+aa++; // 1+1 aa-- -1
+console.log(aa); //2
+console.log(aa++); // 2
+console.log(aa++); // 3
+console.log(aa); // 4
+
+
+
+console.log('Префіксний інкремент');
+console.log(bb); // 1
+++bb; // 1+1  bb-- -1
+console.log(bb); //2
+console.log(++bb); // 3
+console.log(++bb); // 4
+
+// Отримати кільксть товарі що купив користувач і ціну товара 2 prompt   q * price  alert console.log
+
+
+// fed-pro-2206-2023
---
+++ 01/my-script.js
@@ -0,0 +1,64 @@
+// alert('Hello from file'); Ctrl + / 
+
+var oldVariable = 'old'; // old variable ЯКА МАЄ ПРОБЛЕМИ З ВИКОРИСТАННЯМ 
+let newVariable = 'new';
+const restrictNewVariable = 'restrict new';
+
+let username = 'Antonio Vivaldi'; // a, b, c, d, e, f, g, h, i, j 
+let Username = 'Bob';
+let UserName = 'Diana';
+let userName = 'John';
+let user_name = 'Dmitrovia';
+const KEYVALUES ='12324566778';
+let user1 = 'Tom';
+let user2 = 'Jerry';
+
+let user = 'John';
+let consta = 'value';
+
+// ===== string Data ====
+ let stringData = "string data"; 
+ let userOrder ='3';
+
+ console.log(stringData);
+ console.log('stringData print me too');
+
+//  alert("Alert strin");
+//  alert(username);
+
+
+//   === Number =====
+
+let productPrice = 1000;
+console.log(productPrice);
+let numberFloat = 0.002;
+let infinityValue = Infinity;
+let notAnumber = NaN;
+let maxValue = Number.MAX_VALUE;
+let minValue = Number.MIN_VALUE;
+console.log(maxValue);
+console.log(minValue);
+
+//  ===== Boolean =====
+
+let isJsCourse = true;
+let isHtmlCourse = false;
+
+console.log(1<2);
+
+let totalPrice = userName + productPrice;
+console.log(totalPrice);
+
+// ==== null & undefined =====
+
+// console.log(newVariableName);
+
+let elemtn = document.querySelector('li');
+console.log(elemtn);
+
+// let firstValue =1;
+// let seconValue = 2;
+// let summ = firstValue + seconValue;
+
+let dayInfo = prompt("Як ваш день?");
+console.log(dayInfo);
